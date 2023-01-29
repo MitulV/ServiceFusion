cb4a3ad6942f14474efe8540d349e061e45d5248
Mitul <mitulvpatel2211@gmail.com>
2023-01-29 09:54:21 +0530

    dd test

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -48,6 +48,8 @@ class FileUploadController extends Controller
         ]);
 
         $data=json_decode($response,true);
+        
+        dd($data);
 
         $internalJobs=json_decode($data['internal_jobs'],true);
         $contractJobs=json_decode($data['contract_jobs'],true);
