3e8236095c7eb35607fa2d6e3bd924b4e539e7b1
Mitul <mitulvpatel2211@gmail.com>
2023-01-29 20:51:14 +0530

    internal jobs code change done

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Http;
 use Carbon\Carbon;
 use App\Core\CommonUtil;
 use App\Models\SFToken;
-
+use Illuminate\Support\Facades\Log;
 
 class FileUploadController extends Controller
 {
@@ -23,7 +23,6 @@ class FileUploadController extends Controller
         ];
         
         $res=CommonUtil::callAPI($url,json_encode($data),'POST',$tokenOBJ->access_token); 
-
         $tokenOBJ->update([
             "access_token"=>$res['access_token'],
             "refresh_token"=>$res['refresh_token']
@@ -35,7 +34,6 @@ class FileUploadController extends Controller
 
     public function fileUploadPost(Request $request)
     {
-
         $request->validate([
             'file' => 'required|mimes:xlsx',
         ]);
@@ -48,15 +46,51 @@ class FileUploadController extends Controller
         ]);
 
         $data=json_decode($response,true);
-        
-
         $internalJobs=json_decode($data['internal_jobs'],true);
         $contractJobs=json_decode($data['contract_jobs'],true);
         $customerName=json_decode($data['customer_name'],true);
         unset($internalJobs['total hours']);
-       
+
         $accessToken=$this->refreshAccessToken($request);
-       
+
+        $this->internalJobFunction($internalJobs,$customerName,$accessToken);
+
+        //$this->contractJobFunction($contractJobs,$customerName,$accessToken);
+
+        return back()->with('success', 'All Jobs Scheduled Successfully');
+
+    }
+
+    public function contractJobFunction($contractJobs,$customerName,$accessToken){
+        dd($contractJobs);
+        $finalContractJobs=[];
+        foreach ($contractJobs as $key => $job) {
+            if($job=="no jobs scheduled" || $job['task service']==""){
+                continue;    
+            }
+            
+            $noOfJobs=count($job['task category']);
+
+            for ($i=0; $i < $noOfJobs; $i++) { 
+                $obj['customer_name']="Sanay Patel Test";//$customerName
+                $obj['category']="Recurring Maintenance";
+                $obj['status']="Unscheduled";
+                $obj['priority']="Normal";
+                $obj['description']=$job['task service'][$i];
+                
+                $currentYear = Carbon::now()->year;
+                $date = Carbon::createFromFormat('F d', $key." 30");
+                $obj['start_date']=$date->year($currentYear)->format('Y-m-d');
+               
+                //$response = Http::withToken($accessToken)->post('https://api.servicefusion.com/v1/jobs', $obj);
+                //dd($response->getBody()->getContents());
+               
+            }
+        }
+    }
+    
+    public function internalJobFunction($internalJobs,$customerName,$accessToken){
+        $finalInternalJobs=[];
         foreach ($internalJobs as $key => $job) {
             if($job=="no jobs scheduled"){
                 continue;    
@@ -69,43 +103,64 @@ class FileUploadController extends Controller
                 $obj['customer_name']="Sanay Patel Test";//$customerName
                 $obj['category']="Recurring Maintenance";
                 $obj['status']="Unscheduled";
-                $obj['duration']=(int)$job['hours'][$i];
+                $obj['duration']=$job['hours'][$i];
                 $obj['priority']="Normal";
                 $obj['description']=$job['task service'][$i];
                 
                 $currentYear = Carbon::now()->year;
                 $date = Carbon::createFromFormat('F d', $job['date'][$i]);
                 $obj['start_date']=$date->year($currentYear)->format('Y-m-d');
-                $response = Http::withToken($accessToken)->post('https://api.servicefusion.com/v1/jobs', $obj);
+                array_push($finalInternalJobs,$obj);
             }
         }
+        $this->postJobs($this->splitInternalArray($finalInternalJobs),$accessToken);
+    }
 
-        foreach ($contractJobs as $key => $job) {
-            if($job=="no jobs scheduled"){
-                continue;    
-            }
-            
-            $noOfJobs=count($job['task category']);
+    public function splitInternalArray($array){
 
-            for ($i=0; $i < $noOfJobs; $i++) { 
-                $obj['customer_name']="Sanay Patel Test";//$customerName
-                $obj['category']="Recurring Maintenance";
-                $obj['status']="Unscheduled";
-                //$obj['duration']=(int)$job['hours'][$i];
-                $obj['priority']="Normal";
-                $obj['description']=$job['task service'][$i];
-                
-                $currentYear = Carbon::now()->year;
-                $date = Carbon::createFromFormat('F d', $key." 30");
-                $obj['start_date']=$date->year($currentYear)->format('Y-m-d');
-               
-                $response = Http::withToken($accessToken)->post('https://api.servicefusion.com/v1/jobs', $obj);
-                //dd($response->getBody()->getContents());
-               
-            }
+        $mergedArray = [];
+
+        foreach ($array as $item) 
+        {
+        $startDate = $item['start_date'];
+        $description = $item['description'];
+        $customerName = $item['customer_name'];
+        $category = $item['category'];
+        $status = $item['status'];
+        $duration = $item['duration'];
+        $priority = $item['priority'];
+
+        if (isset($mergedArray[$startDate])) {
+            $mergedArray[$startDate]['description'] .= '; Maintenance ' . $description;
+            $mergedArray[$startDate]['duration'] += $duration;
+            $mergedArray[$startDate]['tasks'][] = [
+                'description' => $description,
+                'is_completed' => false,
+              ];
+        } else {
+            $mergedArray[$startDate]['start_date'] = $startDate;
+            $mergedArray[$startDate]['customer_name'] = $customerName;
+            $mergedArray[$startDate]['category'] = $category;
+            $mergedArray[$startDate]['status'] = $status;
+            $mergedArray[$startDate]['priority'] = $priority;
+            $mergedArray[$startDate]['description'] = 'Maintenance '.$description;
+            $mergedArray[$startDate]['duration'] = $duration;
+            $mergedArray[$startDate]['tasks'][] = [
+                'description' => $description,
+                'is_completed' => false,
+              ];
+        }
         }
 
-        return back()->with('success', 'All Jobs Scheduled Successfully');
+        $mergedArray = array_values($mergedArray);
+        return $mergedArray;
+    }
 
+    public function postJobs(Array $jobs,$accessToken){ 
+      dd($jobs);
+        foreach ($jobs as $job) {
+            $job['duration']=(int)$job['duration'];
+            $response=Http::withToken($accessToken)->post('https://api.servicefusion.com/v1/jobs', $job);
+        }     
     }
 }
