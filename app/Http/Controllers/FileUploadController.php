6f23f7ef4f3d595194c5a1869684ce39085fefc8
Mitul <mitulvpatel2211@gmail.com>
2023-03-16 19:44:09 +0530

    store response in excel and auto download

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Http;
 use Carbon\Carbon;
 use App\Core\CommonUtil;
 use App\Models\SFToken;
+use Rap2hpoutre\FastExcel\FastExcel;
 use Illuminate\Support\Facades\Log;
 
 class FileUploadController extends Controller
@@ -46,14 +47,90 @@ class FileUploadController extends Controller
         ]);
 
         $data=json_decode($response,true);
+        
         $internalJobs=json_decode($data['internal_jobs'],true);
         $contractJobs=json_decode($data['contract_jobs'],true);
         $customerName=json_decode($data['customer_name'],true);
         unset($internalJobs['total hours']);
         $accessToken=$this->refreshAccessToken($request);
-        $this->internalJobFunction($internalJobs,$customerName,$accessToken);
+        
+        $internalRes=$this->internalJobFunction($internalJobs,$customerName,$accessToken);
 
-        $this->contractJobFunction($contractJobs,$customerName,$accessToken);
+        $contractRes=$this->contractJobFunction($contractJobs,$customerName,$accessToken);
+        
+       
+
+    $csvArray=[];
+   $i=1;
+    foreach ($internalRes as $value) { 
+            $csvArray[$i]['Job Type']='Internal';            
+            foreach ($value as $k => $v) {
+                if($k!='tasks' && $k!='response' && $k!='duration'){
+                    $csvArray[$i][$k]=$v;
+                }
+            }
+            $i++;
+    }
+
+    foreach ($contractRes as $value) {
+        $csvArray[$i]['Job Type']='Contract';            
+            foreach ($value as $k => $v) {
+                if($k!='tasks' && $k!='response' && $k!='duration'){
+                    $csvArray[$i][$k]=$v;
+                }
+            }
+            $i++;
+    }
+
+
+        $fileName = time().'.'.$request->file('file')->extension(); 
+        $filePath=(new FastExcel($csvArray))->export(storage_path('app/xlsx/'.$fileName));
+        $headers = ['Content-Type: application/xlsx'];
+    	        
+        return response()->download($filePath, $fileName, $headers);
+
+        $failedjobs=[];
+        $totalInternal=count($internalRes);
+        $internalSuccess=0;
+        $internalFail=0;
+        foreach ($internalRes as $internal) {
+            if($internal['response_status']=='success'){
+                $internalSuccess+=1;
+            }else{
+                //dd(($internal['response'])->getBody()->getContents());
+                unset($internal['response']);
+                unset($internal['tasks']);
+                $internal['Job Type']='Internal';
+                array_push($failedjobs,$internal);
+                $internalFail+=1;
+            }
+        }
+
+        $totalContract=count($contractRes);
+        $contractSuccess=0;
+        $contractFail=0;
+        foreach ($contractRes as $contract) {
+            if($contract['response_status']=='success'){
+                $contractSuccess+=1;
+            }else{
+                $contract['Job Type']='Contract';
+                array_push($failedjobs,$contract);
+                $contractFail+=1;
+            }
+        }
+
+        // Here you will get list of failed jobs with $failedjobs variable.
+
+        $reportData=[
+            'Total Jobs'=>$totalInternal+$totalContract,
+            'Total Internal Jobs'=>$totalInternal,
+            'Internal Successfully scheduled jobs'=>$internalSuccess,
+            'Internal Failed scheduled jobs'=>$internalFail,
+            'Total Contract Jobs'=>$totalContract,
+            'Contract Successfully scheduled jobs'=>$contractSuccess,
+            'Contract Failed scheduled jobs'=>$contractFail];
+
+            dd($reportData);
 
         return back()->with('success', 'All Jobs Scheduled Successfully');
 
@@ -82,12 +159,9 @@ class FileUploadController extends Controller
                 $date = Carbon::createFromFormat('F d', $job['date'][$i]);
                 $obj['start_date']=$date->year($currentYear)->format('Y-m-d');
                 array_push($finalContractJobs,$obj);
-               
-                //dd($response->getBody()->getContents());
-               
             }
         }
-        $this->postJobs($finalContractJobs,$accessToken);
+        return $this->postJobs($finalContractJobs,$accessToken);
     }
     
     public function internalJobFunction($internalJobs,$customerName,$accessToken){
@@ -108,33 +182,32 @@ class FileUploadController extends Controller
                 $obj['duration']=$job['hours'][$i];
                 $obj['priority']="Normal";
                 $obj['description']=$job['task service'][$i];
-                
+               
                 $currentYear = Carbon::now()->year;
                 $date = Carbon::createFromFormat('F d', $job['date'][$i]);
                 $obj['start_date']=$date->year($currentYear)->format('Y-m-d');
                 array_push($finalInternalJobs,$obj);
             }
         }
-        
-        $this->postJobs($this->splitInternalArray($finalInternalJobs),$accessToken);
+       return $this->postJobs($this->splitInternalArray($finalInternalJobs),$accessToken);
     }
 
     public function splitInternalArray($array){
-
+       
         $mergedArray = [];
 
         foreach ($array as $item) 
         {
-            $startDate = $item['start_date'];
-            $description = $item['description'];
             $customerName = $item['customer_name'];
             $category = $item['category'];
             $status = $item['status'];
             $duration = $item['duration'];
             $priority = $item['priority'];
+            $description = $item['description'];
+            $startDate = $item['start_date'];
 
             if((isset($mergedArray['category']) && ($mergedArray[$startDate]['category']=='landscaping' || $mergedArray[$startDate]['category']=='pest control' || $mergedArray[$startDate]['category']=='pool maintenance'))){
-                $mergedArray[$startDate]['start_date'] = $startDate;
+                
                     $mergedArray[$startDate]['customer_name'] = $customerName;
                     $mergedArray[$startDate]['category'] = $category;
                     $mergedArray[$startDate]['status'] = $status;
@@ -145,6 +218,7 @@ class FileUploadController extends Controller
                         'description' => $description,
                         'is_completed' => false,
                     ];
+                    $mergedArray[$startDate]['start_date'] = $startDate;
             }else{
                 if (isset($mergedArray[$startDate])) {
                     $mergedArray[$startDate]['description'] .= ' chr(13) Maintenance: ' . $description;
@@ -154,7 +228,7 @@ class FileUploadController extends Controller
                         'is_completed' => false,
                     ];
                 } else {
-                    $mergedArray[$startDate]['start_date'] = $startDate;
+                   
                     $mergedArray[$startDate]['customer_name'] = $customerName;
                     $mergedArray[$startDate]['category'] = $category;
                     $mergedArray[$startDate]['status'] = $status;
@@ -165,13 +239,12 @@ class FileUploadController extends Controller
                         'description' => $description,
                         'is_completed' => false,
                     ];
+                    $mergedArray[$startDate]['start_date'] = $startDate;
                 }
             }
 
-            
-
         }
-        
+
         foreach ($mergedArray as $key => $value) {
             $tasks=$value['tasks'];
             array_push($tasks,[
@@ -200,12 +273,22 @@ class FileUploadController extends Controller
     }
 
     public function postJobs(Array $jobs,$accessToken){ 
+       $response=[];
         foreach ($jobs as $job) {
             if(isset($job['duration'])){
                 $job['duration']=$job['duration'] * 60 * 60;
             }
             
-            $response=Http::withToken($accessToken)->post('https://api.servicefusion.com/v1/jobs', $job);
-        }     
+            $res=Http::withToken($accessToken)->post('https://api.servicefusion.com/v1/jobs', $job);
+            
+            $job['response']=$res;
+            if ($res->getStatusCode() >= 200 && $res->getStatusCode() < 300) {
+                $job['Response_status']='successfully created';
+            } else {
+                $job['Response_status']='failed';
+            }
+            array_push($response,$job);
+        }    
+        return $response; 
     }
 }
